58f979639a818998eb92b058c2a928fca4234e6b
Aditya <121721549+adityaSalame@users.noreply.github.com>
2023-07-03 11:09:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+let f=document.getElementsByName("fname");
+let l=document.getElementsByName("lname");
+let btn=document.getElementsByTagName("input");
+btn[2].addEventListener("click",getFormvalue);
 function getFormvalue() {
     //Write your code here
+	alert(f[0].value+" "+l[0].value);
 
 }
